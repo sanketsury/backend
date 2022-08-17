9893921d1725c3c12bdf467bd2f8e15b5c281f81
sanketsury <sanketsuryawanshi798@gmail.com>
2022-08-17 17:57:52 +0530

    book assignment

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema( {
-    firstName: String,
-    lastName: String,
-    mobile: {
-        type: String,
-        unique: true,
-        required: true
-    },
-    emailId: String,
-    gender: {
-        type: String,
-        enum: ["male", "female", "LGBTQ"] //"falana" will give an error
-    },
-    age: Number,
+
+// const bookSchema = new mongoose.Schema({
+//     bookName: string,
+
+//    bookAuthor: string,
+
+//      catagory:string,
+     
+//      year:Number
+
+
+// },{ timestamps: true });
+
+// const userSchema = new mongoose.Schema( {
+//     firstName: String,
+//     lastName: String,
+//     mobile: {
+//         type: String,
+//         unique: true,
+//         required: true
+//     },
+//     emailId: String,
+//     gender: {
+//         type: String,
+//         enum: ["male", "female", "LGBTQ"] //"falana" will give an error
+//     },
+//     age: Number,
     // isIndian: Boolean,
     // parentsInfo: {
     //     motherName: String,
@@ -21,11 +34,20 @@ const userSchema = new mongoose.Schema( {
     //     siblingName: String
     // },
     // cars: [ String  ]
-}, { timestamps: true });
+//  }, { timestamps: true });
 
-module.exports = mongoose.model('User', userSchema) //users
+//  module.exports = mongoose.model('User', userSchema) //users
+const bookSchema = new mongoose.Schema({
+    bookName: string,
+
+   bookAuthor: string,
+
+     catagory:string,
+     
+     year:Number
 
 
-
+},{ timestamps: true });
 // String, Number
 // Boolean, Object/json, array
+module.exports = mongoose.model('book', bookSchema)
--- src/routes/route.js
+++ src/routes/route.js
@@ -7,8 +7,8 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+router.post("/newBook", UserController.newBook)
 
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getAllbook", UserController.getAllbook)
 
 module.exports = router;
