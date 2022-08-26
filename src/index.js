bb662a02cdd4c5c71865ef6144de835968876bbf
sanketsury <sanketsuryawanshi798@gmail.com>
2022-08-26 13:03:39 +0530

    add middleware-1

--- src/index.js
+++ src/index.js
@@ -2,26 +2,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+const moment=require('moment')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://sanketsuryawanshi:OwiXFmeJvUfout2G@cluster0.k6oib1b.mongodb.net/?retryWrites=true&w=majority",
+{
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-// app.use (
-//     function (req, res, next) {
-//         console.log ("inside GLOBAL MW");
-//         next();
-//   }
-//   );
-  
-
+app.use (
+    function (req, res, next) {
+        const time =moment ().format ("YYYY-MM-DD hh:mm")
+        const ip = req.socket .localAddress
+        const path = req.url
+        console.log ("you are in middleware")
+        console.log (time ,ip ,path)
+        next();
+  }
+  );
 app.use('/', route);
 
 
