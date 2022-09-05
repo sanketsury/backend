d1b4f788989d0f8a7551ce22e2d589c3cd756ac0
satyajit <satyajitbera1796@gmail.com>
2022-09-05 16:16:12 +0530

    project-1

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express= require("express")
+const mongoose = require("mongoose")
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+mongoose.connect("mongodb+srv://satyajitbera1796:18LuJ6umpLy3A35i@cluster0.puyfnq4.mongodb.net/Group65?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
+
+
